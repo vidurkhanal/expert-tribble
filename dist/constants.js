b7700632924a63984fd10b28839809577879a644
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-29 18:42:44 -0400

    constants and types

--- dist/constants.js
+++ dist/constants.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.__prod__ = exports.__PORT__ = void 0;
+exports.ADMIN_PWD = exports.TOKEN_NAME = exports.__prod__ = exports.__PORT__ = void 0;
 exports.__PORT__ = 3000;
 exports.__prod__ = process.env.NODE_ENV === "production";
+exports.TOKEN_NAME = "auth_token";
+exports.ADMIN_PWD = process.env.ADMIN_PASSWORD;
 //# sourceMappingURL=constants.js.map
