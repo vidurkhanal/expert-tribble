6914b01f99fa65293f81de68adf75ab523035145
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-30 12:31:56 -0400

    types and constants added

--- dist/constants.js
+++ dist/constants.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ADMIN_PWD = exports.TOKEN_NAME = exports.__prod__ = exports.__PORT__ = void 0;
+exports.COOKIE_NAME = exports.ADMIN_PWD = exports.__prod__ = exports.__PORT__ = void 0;
 exports.__PORT__ = 3000;
 exports.__prod__ = process.env.NODE_ENV === "production";
-exports.TOKEN_NAME = "auth_token";
 exports.ADMIN_PWD = process.env.ADMIN_PASSWORD;
+exports.COOKIE_NAME = "exodus_authKey";
 //# sourceMappingURL=constants.js.map
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
 import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
+import { RedisClient } from "redis";
 
-export type betterRequest = Request & { userID: string };
+export type betterRequest = Request & {
+  session: Session & Partial<SessionData> & { userId?: string };
+};
 export type betterResponse = Response;
+export type betterRedis = RedisClient;
