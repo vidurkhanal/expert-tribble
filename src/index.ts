9ec2a233208167f7be83eaafcf7761e4fef85788
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-30 19:37:36 -0400

    origin added to cors

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const main = async () => {
 
   // INITIALIZE THE APP
   const app = express();
-  app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
   app.use(
     session({
